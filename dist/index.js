cdd61fa4c390d300c7d47557e4a9b9dc0cc5ea2e
vineethpace <vineeth.p@pacewisdom.com>
2023-10-17 16:22:28 +0530

    getter and setter

--- dist/index.js
+++ dist/index.js
@@ -1,11 +1,41 @@
 "use strict";
-let age = 20;
-const isYoung = (age) => {
-    if (age < 60)
-        return true;
-    else
-        return false;
+class Account {
+    constructor(id, owner, balance) {
+        this.id = id;
+        this.owner = owner;
+        this._balance = balance;
+    }
+    deposit(amount) {
+        if (amount < 0)
+            throw new Error('Invalid Amount');
+        this._balance += amount;
+    }
+    //balance getter
+    get balance() {
+        return this._balance;
+    }
+    //balance setter
+    set balance(value) {
+        if (value < 0)
+            throw new Error('Invalid Amount');
+        this._balance = value;
+    }
+}
+const main = () => {
+    try {
+        const account = new Account(1, 'vineeth', 100);
+        //error - id is readonly
+        // account.id =  2
+        //error - balance is private to Account
+        // console.log(account._balance)
+        account.balance = 30; //balance setter
+        console.log(account.balance); //getter access
+        console.log(account);
+        console.log(account instanceof Account);
+    }
+    catch (err) {
+        console.log(err);
+    }
 };
-const resultAge = isYoung(age);
-console.log(resultAge);
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,60 @@
-let age: number = 20;
+class Account {
 
-const isYoung = (age: number): boolean => {
-    if (age < 60)
-        return true;
-    else
-        return false;
+    readonly id: number;
+    owner: string;
+    private _balance: number;
+    nickname?: string; //optional property - ?
+
+    constructor(id: number, owner: string, balance: number) {
+        this.id = id;
+        this.owner = owner;
+        this._balance = balance;
+    }
+
+    deposit(amount: number) {
+        if (amount < 0)
+            throw new Error('Invalid Amount')
+        this._balance += amount
+    }
+
+    //balance getter
+    get balance(): number {
+        return this._balance
+    }
+
+    //balance setter
+    set balance(value: number) {
+        if (value < 0)
+            throw new Error('Invalid Amount')
+        this._balance = value
+    }
+
 }
 
-const resultAge: boolean = isYoung(age);
+const main = () => {
 
-console.log(resultAge)
+    try {
+        const account = new Account(1, 'vineeth', 100)
+
+        //error - id is readonly
+        // account.id =  2
+
+        //error - balance is private to Account
+        // console.log(account._balance)
+
+        account.balance = 30 //balance setter
+
+        console.log(account.balance) //getter access
+
+        console.log(account)
+        console.log(account instanceof Account)
+    }
+    catch (err) {
+        console.log(err)
+    }
+
+
+
+}
+
+main()
