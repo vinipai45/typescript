058100adc18cba2f3aa34ddef939315b57c7f7ea
vinipai45 <vinipai45@gmail.com>
2023-10-18 11:39:14 +0530

    generic classes

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,10 @@
-//abstract class - creates an explicit class on compiling in js
-// abstract class Calender {
-//     constructor(
-//         public name: string,
-//     ) { }
-
-//     abstract addEvent(): void;
-//     abstract removeEvent(): void;
-// }
-
-//interface - for optimised compiling and js conversion
-
-//if there are any methods in base class that subclass needs to use - use abstract class
-//else if only signatures -  use interfaces
-
-interface Calender {
-    name: string;
-    addEvent(): void;
-    removeEvent(): void;
+class KeyValuePair<TKey, TValue> {
+	constructor(public key: TKey, public value: TValue) {}
 }
 
-interface CloudCalendar extends Calender {
-    sync(): void;
-}
+const main = () => {
+	//type of members are generic based on value passed
+	const kv1 = new KeyValuePair(1, 1);
+};
 
-class GoogleCalendar implements CloudCalendar {
-
-    constructor(public name: string) { }
-
-    sync(): void {
-        throw new Error("Method not implemented.");
-    }
-
-    addEvent(): void {
-        throw new Error("Method not implemented.");
-    }
-
-    removeEvent(): void {
-        throw new Error("Method not implemented.");
-    }
-
-}
+main();
