05c5fcf6bbd6421f3bebc13111c6bab2acf18743
vinipai45 <vinipai45@gmail.com>
2023-10-18 13:25:50 +0530

    generic functions or methods

--- dist/index.js
+++ dist/index.js
@@ -1,7 +1,13 @@
 "use strict";
-class Calender {
-    constructor(name) {
-        this.name = name;
+class ArrayUtils {
+    static wrapperInArray(value) {
+        return [value];
     }
 }
+const main = () => {
+    //type of members are generic based on value passed
+    const kv1 = ArrayUtils.wrapperInArray(1);
+    console.log(kv1);
+};
+main();
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
-class KeyValuePair<TKey, TValue> {
-	constructor(public key: TKey, public value: TValue) {}
+class ArrayUtils {
+	static wrapperInArray<T>(value: T) {
+		return [value];
+	}
 }
 
 const main = () => {
 	//type of members are generic based on value passed
-	const kv1 = new KeyValuePair(1, 1);
+	const kv1 = ArrayUtils.wrapperInArray(1);
+
+	console.log(kv1);
 };
 
 main();
